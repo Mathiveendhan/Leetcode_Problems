63674e2fc5c11209c35103acf9868554078968ce
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2024-08-12 20:53:11 +0530

    Time: 0 ms (100%), Space: 40.3 MB (97.28%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,14 +1,20 @@
 class Solution {
-    public boolean isPowerOfFour(int n) {
-        if(n==2147483647){
-            return false;
-        }
-        for(int i=0;i<32;i++){
-            int num=(int)Math.pow(4,i);
-            if(num==n){
-                return true;
-            }
-        }
-        return false;
-    }
-}
+     public boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n % 3 == 1);
+     }
+ }
+
+// class Solution {
+//     public boolean isPowerOfFour(int n) {
+//         if(n==2147483647){
+//             return false;
+//         }
+//         for(int i=0;i<32;i++){
+//             int num=(int)Math.pow(4,i);
+//             if(num==n){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
