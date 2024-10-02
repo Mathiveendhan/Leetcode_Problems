5565ad832ff27d1b9861ddbc7488f00da58977db
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2024-10-02 21:30:39 +0530

    Time: 4 ms (60.39%), Space: 42.8 MB (51.86%) - LeetHub

---
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public char kthCharacter(int k) {
+        
+        String s = "a";
+            
+        while (s.length() < k) {
+            StringBuilder temp = new StringBuilder();
+            for (char c : s.toCharArray()) {
+                if (c == 'z') {
+                    temp.append('a');
+                }
+                else {
+                    temp.append((char) (c + 1));
+                }
+            }
+            s += temp.toString();
+        }
+            
+        return s.charAt(k - 1);
+    
+    }
+}
