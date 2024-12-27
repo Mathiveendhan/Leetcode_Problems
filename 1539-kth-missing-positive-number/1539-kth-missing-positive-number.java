0440b82804fee8af80cd988d71726a7a8b0982c8
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2024-12-27 12:19:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        ArrayList<Integer> num=new ArrayList<>();
+        for(int w:arr){
+            num.add(w);
+        }
+        int a=k;
+        for(int i=1;i<1001;i++){
+            if(!num.contains(i)){
+                a--;
+                if(a==0){
+                    return i;
+                }
+            }
+          
+        }
+          return 0;
+    }
+}
