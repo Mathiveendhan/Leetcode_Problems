fe274050a416684a0d2617222c2ce8aea28f5f25
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-01-02 11:01:05 +0530

    Time: 66 ms (36.03%), Space: 45.8 MB (29.52%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,26 +1,18 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
         if (word1.length() != word2.length()) {
-            return false;
+            return false; 
         }
 
         HashMap<Character, Integer> m1 = new HashMap<>();
         HashMap<Character, Integer> m2 = new HashMap<>();
 
         for (char c : word1.toCharArray()) {
-            if (!m1.containsKey(c)) {
-                m1.put(c, 1);
-            } else {
-                m1.put(c, m1.get(c) + 1);
-            }
+            m1.put(c, m1.getOrDefault(c, 0) + 1); 
         }
 
         for (char cc : word2.toCharArray()) {
-            if (!m2.containsKey(cc)) {
-                m2.put(cc, 1);
-            } else {
-                m2.put(cc, m2.get(cc) + 1);
-            }
+            m2.put(cc, m2.getOrDefault(cc, 0) + 1);
         }
 
         if (!m1.keySet().equals(m2.keySet())) {
@@ -33,6 +25,46 @@ class Solution {
         Collections.sort(arr1);
         Collections.sort(arr2);
 
-        return arr1.equals(arr2);
+        return arr1.equals(arr2); 
     }
 }
+
+
+// class Solution {
+//     public boolean closeStrings(String word1, String word2) {
+//         if (word1.length() != word2.length()) {
+//             return false;
+//         }
+
+//         HashMap<Character, Integer> m1 = new HashMap<>();
+//         HashMap<Character, Integer> m2 = new HashMap<>();
+
+//         for (char c : word1.toCharArray()) {
+//             if (!m1.containsKey(c)) {
+//                 m1.put(c, 1);
+//             } else {
+//                 m1.put(c, m1.get(c) + 1);
+//             }
+//         }
+
+//         for (char cc : word2.toCharArray()) {
+//             if (!m2.containsKey(cc)) {
+//                 m2.put(cc, 1);
+//             } else {
+//                 m2.put(cc, m2.get(cc) + 1);
+//             }
+//         }
+
+//         if (!m1.keySet().equals(m2.keySet())) {
+//             return false;
+//         }
+
+//         ArrayList<Integer> arr1 = new ArrayList<>(m1.values());
+//         ArrayList<Integer> arr2 = new ArrayList<>(m2.values());
+
+//         Collections.sort(arr1);
+//         Collections.sort(arr2);
+
+//         return arr1.equals(arr2);
+//     }
+// }
