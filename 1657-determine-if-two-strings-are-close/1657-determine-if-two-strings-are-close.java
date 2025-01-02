77008c3d843881d59c871f7efa2d14e7d31a9eff
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-01-02 11:00:55 +0530

    Time: 84 ms (13.19%), Space: 45.9 MB (20.42%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,41 +1,38 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if(word1.length()!=word2.length()){
+        if (word1.length() != word2.length()) {
             return false;
         }
-        HashMap<Character,Integer> m1=new HashMap<>();
-        HashMap<Character,Integer> m2=new HashMap<>();
 
-        for(char c:word1.toCharArray()){
-           if(!m1.containsKey(c)){
-            m1.put(c,1);
-           }else{
-            m1.put(c,m1.get(c)+1);
-           }
+        HashMap<Character, Integer> m1 = new HashMap<>();
+        HashMap<Character, Integer> m2 = new HashMap<>();
+
+        for (char c : word1.toCharArray()) {
+            if (!m1.containsKey(c)) {
+                m1.put(c, 1);
+            } else {
+                m1.put(c, m1.get(c) + 1);
+            }
         }
 
-        for(char cc:word2.toCharArray()){
-           if(!m2.containsKey(cc)){
-            m2.put(cc,1);
-           }else{
-            m2.put(cc,m2.get(cc)+1);
-           }
+        for (char cc : word2.toCharArray()) {
+            if (!m2.containsKey(cc)) {
+                m2.put(cc, 1);
+            } else {
+                m2.put(cc, m2.get(cc) + 1);
+            }
         }
 
-        ArrayList<Integer> arr1=new ArrayList<>();
-        ArrayList<Integer> arr2=new ArrayList<>();
-
-        for(int i:m1.keySet()){
-            arr1.add(i);
+        if (!m1.keySet().equals(m2.keySet())) {
+            return false;
         }
 
-        for(int j:m2.keySet()){
-            arr2.add(j);
-        }
+        ArrayList<Integer> arr1 = new ArrayList<>(m1.values());
+        ArrayList<Integer> arr2 = new ArrayList<>(m2.values());
 
         Collections.sort(arr1);
         Collections.sort(arr2);
 
         return arr1.equals(arr2);
-        }
+    }
 }
