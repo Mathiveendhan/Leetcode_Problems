ca24b4f80f1bd043466d245bf6eac633151501fd
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-01-02 10:59:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,40 +1,41 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if (word1.length() != word2.length()) {
+        if(word1.length()!=word2.length()){
             return false;
         }
-        
-        HashMap<Character, Integer> m1 = new HashMap<>();
-        HashMap<Character, Integer> m2 = new HashMap<>();
+        HashMap<Character,Integer> m1=new HashMap<>();
+        HashMap<Character,Integer> m2=new HashMap<>();
 
-        // Count frequencies for word1
-        for (char c : word1.toCharArray()) {
-            m1.put(c, m1.getOrDefault(c, 0) + 1);
+        for(char c:word1.toCharArray()){
+           if(!m1.containsKey(c)){
+            m1.put(c,1);
+           }else{
+            m1.put(c,m1.get(c)+1);
+           }
         }
 
-        // Count frequencies for word2
-        for (char cc : word2.toCharArray()) {
-            m2.put(cc, m2.getOrDefault(cc, 0) + 1);
+        for(char cc:word2.toCharArray()){
+           if(!m2.containsKey(cc)){
+            m2.put(cc,1);
+           }else{
+            m2.put(cc,m2.get(cc)+1);
+           }
         }
 
-        // Check if both maps have the same keys (unique characters)
-        if (!m1.keySet().equals(m2.keySet())) {
-            return false;
+        ArrayList<Integer> arr1=new ArrayList<>();
+        ArrayList<Integer> arr2=new ArrayList<>();
+
+        for(int i:m1.keySet()){
+            arr1.add(i);
         }
 
-        // Create arrays to hold frequency counts
-        ArrayList<Integer> arr1 = new ArrayList<>(m1.values());
-        ArrayList<Integer> arr2 = new ArrayList<>(m2.values());
+        for(int j:m2.keySet()){
+            arr2.add(j);
+        }
 
-        // Sort frequency arrays
         Collections.sort(arr1);
         Collections.sort(arr2);
 
-        // Compare sorted frequency counts
         return arr1.equals(arr2);
-    }
+        }
 }
