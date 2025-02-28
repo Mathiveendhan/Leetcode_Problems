a32f738c78cc97bbca121c7844c3f92cec50d72c
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-02-28 15:13:44 +0530

    Time: 1 ms (54.25%), Space: 42.4 MB (96.34%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,44 @@
 import java.util.*;
 
 class Solution {
-    public void call(int idx,Stack<Integer> stack,int n,int[] arr,List<List<Integer>> ans) {
-        if(idx>=n){
-            ans.add(new ArrayList<>(stack));
+    public void call(int idx, List<Integer> list, int n, int[] arr, List<List<Integer>> ans) {
+        if (idx >= n) {
+            ans.add(new ArrayList<>(list));
             return;
         }
-        stack.push(arr[idx]);
-        call(idx+1,stack,n,arr,ans);
-        stack.pop();
-        call(idx+1,stack,n,arr,ans);
+        list.add(arr[idx]);
+        call(idx + 1, list, n, arr, ans);
+        list.remove(list.size() - 1); 
+        call(idx + 1, list, n, arr, ans);
     }
 
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();  
-        Stack<Integer> stack=new Stack<>();
-        call(0,stack,nums.length,nums,ans);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        call(0, list, nums.length, nums, ans);
         return ans;
     }
 }
+
+
+
+// import java.util.*;
+// class Solution {
+//     public void call(int idx,Stack<Integer> stack,int n,int[] arr,List<List<Integer>> ans) {
+//         if(idx>=n){
+//             ans.add(new ArrayList<>(stack));
+//             return;
+//         }
+//         stack.push(arr[idx]);
+//         call(idx+1,stack,n,arr,ans);
+//         stack.pop();
+//         call(idx+1,stack,n,arr,ans);
+//     }
+
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> ans=new ArrayList<>();  
+//         Stack<Integer> stack=new Stack<>();
+//         call(0,stack,nums.length,nums,ans);
+//         return ans;
+//     }
+// }
