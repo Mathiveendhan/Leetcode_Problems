0e64ebfb25f9324bfa5b26926b8372430b588a3f
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-02-28 15:03:49 +0530

    Time: 1 ms (54.25%), Space: 42.9 MB (30.07%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,21 @@
 import java.util.*;
 
 class Solution {
-    public void call(int idx, Stack<Integer> stack, int n, int[] arr, List<List<Integer>> ans) {
-        if (idx >= n) {
+    public void call(int idx,Stack<Integer> stack,int n,int[] arr,List<List<Integer>> ans) {
+        if(idx>=n){
             ans.add(new ArrayList<>(stack));
             return;
         }
         stack.push(arr[idx]);
-        call(idx + 1, stack, n, arr, ans);
+        call(idx+1,stack,n,arr,ans);
         stack.pop();
-        call(idx + 1, stack, n, arr, ans);
+        call(idx+1,stack,n,arr,ans);
     }
 
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();  
-        Stack<Integer> stack = new Stack<>();
-        call(0, stack, nums.length, nums, ans);
+        List<List<Integer>> ans=new ArrayList<>();  
+        Stack<Integer> stack=new Stack<>();
+        call(0,stack,nums.length,nums,ans);
         return ans;
     }
 }
