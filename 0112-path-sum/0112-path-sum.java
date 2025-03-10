952348342f31f2c92ef8db6b22c2ac8d3148e658
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-03-10 14:52:03 +0530

    Time: 131 ms (1.79%), Space: 43.1 MB (60.61%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -1,31 +1,27 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    public boolean call(TreeNode root, int target, int sum) {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) {
             return false;
         }
-        sum += root.val;
         if (root.left == null && root.right == null) {
-            return sum == target;
+            return targetSum == root.val;
         }
-        return call(root.left, target, sum) || call(root.right, target, sum);
-    }
-    
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        return call(root, targetSum, 0);
+        targetSum -= root.val;
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
     }
 }
