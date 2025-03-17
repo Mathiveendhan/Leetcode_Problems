9d2403a6c57abc0946f53cd23bf793107387727a
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-03-17 19:49:06 +0530

    Time: 0 ms (100%), Space: 41.4 MB (15.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,8 +1,10 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
 
         int[] dp = new int[n];
         dp[0] = nums[0];
