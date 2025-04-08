74144e18e8a9940394f431715790df6bf78af8e4
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-04-08 12:54:48 +0530

    Time: 9 ms (12.26%), Space: 45.1 MB (6.29%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,18 +1,18 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-       List<Integer> l1=new ArrayList<>();
-       for(int num:nums){
-        l1.add(num);
+       List<Integer> arr = new ArrayList<>();
+       for (int num : nums) {
+           arr.add(num);
        }
-       int count=0;
-       while(true){
-            Set<Integer> hs=new HashSet<>(l1);
-            if(hs.size()==l1.size()){
+       int count = 0;
+       while (true) {
+            Set<Integer> set = new HashSet<>(arr);
+            if (set.size() == arr.size()) {
                 break;
             }
-            int remCount=Math.min(3,l1.size());
-            for(int i=0;i<remCount;i++){
-                l1.remove(0);
+            int remCount = Math.min(3, arr.size());
+            for (int i = 0; i < remCount; i++) {
+                arr.remove(0);
             }
             count++;
        }
