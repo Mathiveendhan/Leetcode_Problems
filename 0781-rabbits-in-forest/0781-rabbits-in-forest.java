72c93031804698cccc7e22b52e1c10cb91b88b84
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-04-20 17:58:21 +0530

    Time: 3 ms (82.48%), Space: 42.7 MB (17.15%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int numRabbits(int[] answers) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int rabbits = 0;
+        for (int answer : answers) {
+            if (!map.containsKey(answer)) {
+                map.put(answer, 1);
+                rabbits += answer + 1;
+            } else {
+                map.put(answer, map.get(answer) + 1);
+            }
+            if (map.get(answer) == answer + 1) {
+                map.remove(answer);
+            }
+        }
+        return rabbits;
+    }
+}
