3a54ac42a07a77e2949f61e4c16d857dc4b70cc4
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-04-26 07:28:21 +0530

    Time: 6 ms (62.36%), Space: 45 MB (64.08%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,22 +1,43 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> hash = new HashMap<>();
-        for (int i = 0; i < magazine.length(); i++) {
-            char ch = magazine.charAt(i);
-            if (!hash.containsKey(ch)) {
-                hash.put(ch, 1);
-            } else {
-                hash.put(ch, hash.get(ch) + 1);
-            }
+
+        int[] freq=new int[26];
+        for(int i=0;i<magazine.length();i++){
+            char c=Character.toLowerCase(magazine.charAt(i));
+            freq[c-'a']++;
         }
-        for (int j = 0; j < ransomNote.length(); j++) {
-            char chr = ransomNote.charAt(j);
-            if (hash.containsKey(chr) && hash.get(chr)>0) {
-                hash.put(chr, hash.get(chr) - 1);
-            } else {
+        for(int j=0;j<ransomNote.length();j++){
+            char d=Character.toLowerCase(ransomNote.charAt(j));
+            if(freq[d-'a']==0){
                 return false;
+            }else{
+                freq[d-'a']--;
             }
         }
         return true;
     }
 }
+
+
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         HashMap<Character, Integer> hash = new HashMap<>();
+//         for (int i = 0; i < magazine.length(); i++) {
+//             char ch = magazine.charAt(i);
+//             if (!hash.containsKey(ch)) {
+//                 hash.put(ch, 1);
+//             } else {
+//                 hash.put(ch, hash.get(ch) + 1);
+//             }
+//         }
+//         for (int j = 0; j < ransomNote.length(); j++) {
+//             char chr = ransomNote.charAt(j);
+//             if (hash.containsKey(chr) && hash.get(chr)>0) {
+//                 hash.put(chr, hash.get(chr) - 1);
+//             } else {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
