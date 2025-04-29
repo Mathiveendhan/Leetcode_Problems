a20ea9e429e9f9622ee3789059775e557c4cedb4
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-04-29 19:29:44 +0530

    Time: 14 ms (24.15%), Space: 49.3 MB (93.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,44 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = nums[0];
-        int count = 1;
-        
-       
-        for (int i = 1; i < nums.length; i++) {
-            if (count == 0) {
-                candidate = nums[i];
-            }
-            if (nums[i] == candidate) {
-                count++;
-            } else {
-                count--;
-            }
+       HashMap<Integer,Integer> map=new HashMap<>();
+       for(int q:nums){
+        if(!map.containsKey(q)){
+            map.put(q,1);
+        }else{
+            map.put(q,map.get(q)+1);
         }
-       
-        return candidate;
+       }
+      int ans=0;
+      int max=Integer.MIN_VALUE;
+      for(int i:nums){
+        if(max<map.get(i)){
+            max=map.get(i);
+            ans=i;
+        }
+      }
+      return ans;
     }
 }
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int candidate = nums[0];
+//         int count = 1;
+        
+       
+//         for (int i = 1; i < nums.length; i++) {
+//             if (count == 0) {
+//                 candidate = nums[i];
+//             }
+//             if (nums[i] == candidate) {
+//                 count++;
+//             } else {
+//                 count--;
+//             }
+//         }
+       
+//         return candidate;
+//     }
+// }
     
    
