398f9f8aa0efd8ec2f9dbc222faece59c07c7266
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-04-29 19:35:23 +0530

    Time: 13 ms (19.19%), Space: 46.2 MB (46.45%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int q:nums){
+            if(!map.containsKey(q)){
+                map.put(q,1);
+            }else{
+                map.put(q,map.get(q)+1);
+            }
+        }
+        for(int i:nums){
+            if(map.get(i)==1){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int count=0;
+//         for(int i=0;i<nums.length;i++){
+//             int num=nums[i];
+//             for(int j=0;j<nums.length;j++){
+//                 if(num==nums[j]){
+//                      count++;
+//                 }
+//             }
+//             if(count==1){
+//                 return num;
+//             }else{
+//                 count=0;
+//             }
+//         }
+//         return 0;
+//     }
+// }
