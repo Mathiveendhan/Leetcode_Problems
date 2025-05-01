aab154ab3eb64f335ac938cf1f58ff14a570e70f
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-05-01 11:48:12 +0530

    Time: 1 ms (99.59%), Space: 57.5 MB (8.79%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0; 
+        int max = nums[0];
+        
+        for(int i=0; i<nums.length; i++){
+            sum = sum + nums[i];
+            max = Math.max(sum, max);
+
+            if(sum < 0) sum = 0;
+        }
+        
+        return max;
+    }
+}
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int cur=nums[0];
+//         int max=nums[0];
+//         for(int i=1;i<nums.length;i++){
+//             if(cur+nums[i]>nums[i]){
+//                 cur+=nums[i];
+//             }else{
+//                 cur=nums[i];
+//             }
+
+//             if(cur>max){
+//                 max=cur;
+//             }
+//         }
+//         return max;
+//     }
+// }
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int max=Integer.MIN_VALUE;
+//         for(int i=0;i<nums.length;i++){
+//             int sum=0;
+//             for(int j=i;j<nums.length;j++){
+//                 sum+=nums[j];
+//                 max=Math.max(max,sum);
+//             }
+//         }
+//         return max;
+//     }
+// }
