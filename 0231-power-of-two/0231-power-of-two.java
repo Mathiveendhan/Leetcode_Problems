9f8b17ee898076f984e7bc41c0930e29d06619d1
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-05-04 15:34:11 +0530

    Time: 0 ms (100%), Space: 41.1 MB (26.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==2147483647){
-            return false;
-        }
-        for(int i=0;i<32;i++){
-            int num=(int)Math.pow(2,i);
-            if(num==n){
-                return true;
-            }
-        }
-        return false;
-    }
+      return n > 0 && (n & (n - 1)) == 0;
+  }
 }
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if(n==2147483647){
+//             return false;
+//         }
+//         for(int i=0;i<32;i++){
+//             int num=(int)Math.pow(2,i);
+//             if(num==n){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
