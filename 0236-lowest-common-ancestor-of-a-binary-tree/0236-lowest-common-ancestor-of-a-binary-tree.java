dc42802775e1fcd5e08bc55e16fafcf63c7e5cb2
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-06-06 09:57:05 +0530

    Time: 9 ms (15.12%), Space: 45.6 MB (7.28%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,18 +8,82 @@
  * }
  */
 class Solution {
+    public static boolean dfs(ArrayList<TreeNode> arr, TreeNode root, TreeNode p) {
+        if (root == null) {
+            return false;
+        }
+        arr.add(root);
+        if (root.val == p.val) {
+            return true;
+        }
+        if (dfs(arr, root.left, p)) {
+            return true;
+        }
+        if (dfs(arr, root.right, p)) {
+            return true;
+        }
+        arr.remove(arr.size() - 1); 
+        return false;
+    }
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null || root==p || root==q){
-            return root;
+        ArrayList<TreeNode> arr1 = new ArrayList<>();
+        ArrayList<TreeNode> arr2 = new ArrayList<>();
+        dfs(arr1, root, p);
+        dfs(arr2, root, q);
+        int len = Math.min(arr1.size(), arr2.size());
+        TreeNode lastCommon = root;
+        for (int i = 0; i < len; i++) {
+            if (arr1.get(i).val != arr2.get(i).val) {
+                return lastCommon;
+            }
+            lastCommon = arr1.get(i);
         }
-        TreeNode left=lowestCommonAncestor(root.left,p,q);
-        TreeNode right=lowestCommonAncestor(root.right,p,q);
-        if(left==null){
-            return right;
-        }else if(right==null){
-            return left;
-        }else{
-            return root;
-        }
+        return lastCommon;
     }
 }
+
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode(int x) { val = x; }
+//  * }
+//  */
+// class Solution {
+//     public static void dfs(ArrayList<TreeNode> arr,TreeNode root,TreeNode p){
+//         if(root==null){
+//             return;
+//         }
+//         arr.add(root);
+//         if(root.val==p.val){
+//             return;
+//         }
+//         dfs(arr,root.left,p);
+//         arr.remove(arr.size()-1);
+//         dfs(arr,root.right,p);
+//         //arr.remove(arr.size()-1);
+
+//     }
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         ArrayList<TreeNode> arr1=new ArrayList<>();
+//         ArrayList<TreeNode> arr2=new ArrayList<>();
+//         dfs(arr1,root,p);
+//         dfs(arr2,root,q);
+//         int len=Math.min(arr1.size(),arr2.size());
+
+//         // if(arr1.get(len-1).val==arr2.get(len-1).val){
+//         //     return arr2.get(len-1);
+//         // }
+//         System.out.print(len);
+
+//         for(int i=0;i<len;i++){
+//             if((arr1.get(i)).val==(arr2.get(i)).val){
+//                 return arr1.get(i);
+//             }
+//         }
+//         return arr1.get(len-1);
+//     }
+// }
