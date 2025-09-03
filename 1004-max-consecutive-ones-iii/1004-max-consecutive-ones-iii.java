865da3ff0c5de735009bf38f2e4395160961051c
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2025-09-03 15:22:32 +0530

    Time: 4 ms (34.08%), Space: 53.3 MB (13.82%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,20 +1,40 @@
-class Solution {
+ class Solution {
     public int longestOnes(int[] nums, int k) {
-     int l=0,zero=0,maxlen=0;
+        int l=0,zero=0,maxlen=0;
         for(int r=0;r<nums.length;r++){
             if(nums[r]==0){
                 zero++;
             }
             if(zero>k){
-                while(zero>k){
-                    if(nums[l]==0){
-                        zero--;
-                    }
-                    l++;
+                if(nums[l]==0){
+                    zero--;
                 }
+                l++;
             }
-            maxlen=Math.max(maxlen,r-l+1);
+            if(zero<=k){
+                maxlen=Math.max(maxlen,r-l+1);
+            }
         }
-        return maxlen;   
-    }
+        return maxlen;
+   }
 }
+// class Solution {
+//     public int longestOnes(int[] nums, int k) {
+//      int l=0,zero=0,maxlen=0;
+//         for(int r=0;r<nums.length;r++){
+//             if(nums[r]==0){
+//                 zero++;
+//             }
+//             if(zero>k){
+//                 while(zero>k){
+//                     if(nums[l]==0){
+//                         zero--;
+//                     }
+//                     l++;
+//                 }
+//             }
+//             maxlen=Math.max(maxlen,r-l+1);
+//         }
+//         return maxlen;   
+//     }
+// }
