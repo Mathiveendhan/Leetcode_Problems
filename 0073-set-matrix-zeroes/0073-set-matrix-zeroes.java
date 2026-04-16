67393713e02468a8f1b0a3cfe63041151a46f539
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2026-04-16 12:41:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,20 @@
 class Solution {
-     public static void call(int[][] matrix,int row,int col){
-        for(int i=0;i<matrix[0].length;i++){
-            matrix[row][i]=0;
-        }
-
-        for(int j=0;j<matrix.length;j++){
-            matrix[j][col]=0;
-        }
-    }
     public void setZeroes(int[][] matrix) {
-        int[][] check=matrix.clone();
-
-        for(int a=0;a<matrix.length;a++){
-            check[a]=matrix[a].clone();
-        }
-        
+        int[] row=new int[matrix.length];
+        int[] col=new int[matrix[0].length];
 
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0 && check[i][j]==0)  call(matrix,i,j);
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(row[i]==1 || col[j]==1) matrix[i][j]=0;
             }
         }
         
