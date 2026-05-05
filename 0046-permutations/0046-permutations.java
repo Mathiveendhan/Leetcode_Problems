5bcc62978ecf899e00053d30b35b74282fa36a5d
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2026-05-05 12:51:25 +0530

    Time: 1 ms (86.29%), Space: 45.6 MB (46.55%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,25 @@
 class Solution {
-    public static void call(int[] nums,List<List<Integer>> ans,List<Integer> arr,boolean[] vis){
-       if(arr.size()==nums.length){
-        ans.add(new ArrayList<>(arr));
-        return;
-       } 
-       for(int i=0;i<nums.length;i++){
-        if(!vis[i]){
-            vis[i]=true;
-            arr.add(nums[i]);
-            call(nums,ans,arr,vis);
-            vis[i]=false;
-            arr.remove(arr.size()-1);
+    public static void call(int[] nums, List<List<Integer>> ans, List<Integer> arr, boolean[] vis) {
+        if (arr.size() == nums.length) {
+            ans.add(new ArrayList<>(arr));
+            return;
         }
-       }
+        for (int i = 0; i < nums.length; i++) {
+            if (!vis[i]) {
+                vis[i] = true;
+                arr.add(nums[i]);
+                call(nums, ans, arr, vis);
+                vis[i] = false;
+                arr.remove(arr.size() - 1);
+            }
+        }
     }
+
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> arr=new ArrayList<>();
-        boolean[] vis=new boolean[nums.length];
-        call(nums,ans,arr,vis);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+        boolean[] vis = new boolean[nums.length];
+        call(nums, ans, arr, vis);
         return ans;
     }
 }
