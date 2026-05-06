0c8a1be4d4b2c0e67c392d77893e4141596e21a9
MathiV_'-"002v <113008726+Mathiveendhan@users.noreply.github.com>
2026-05-06 12:19:26 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,34 @@
 class Solution {
-    public static int call(int n,HashMap<Integer,Integer> map){
-        if(n==0){
-            return 1;
-        }
-        if (n < 0) return 0; 
-        if (map.containsKey(n)) return map.get(n);
-        int ways = call(n - 1, map) + call(n - 2, map);
-        map.put(n, ways);
-        return ways;
-    }
     public int climbStairs(int n) {
-        return call(n,new HashMap<>());
+            if (n == 0 || n == 1) return 1;
+            
+            int[] dp = new int[n + 1];
+            dp[0] = 1; 
+            dp[1] = 1;
+
+            for (int i = 2; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[n];
     }
 }
+
+// class Solution {
+//     public static int call(int n,HashMap<Integer,Integer> map){
+//         if(n==0){
+//             return 1;
+//         }
+//         if (n < 0) return 0; 
+//         if (map.containsKey(n)) return map.get(n);
+//         int ways = call(n - 1, map) + call(n - 2, map);
+//         map.put(n, ways);
+//         return ways;
+//     }
+//     public int climbStairs(int n) {
+//         return call(n,new HashMap<>());
+//     }
+// }
 
 
 // class Solution {
